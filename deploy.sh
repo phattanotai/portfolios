f97639941eb50311e6a01383e619c65930692966
phattanothai <zeref38@gmail,com>
2021-12-26 19:57:24 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,12 @@
  
 #run docker with docker
 docker-compose build --build-arg VERSION_INFO=$1
+
+# deploy docker
 docker-compose up -d
+
+# deploy docker swarm
+
 # docker stack deploy --compose-file docker-compose.yml react-cicd
-# docker service update react-cicd_react-context-cicd --image react-context-cicd --force
+# docker service update portfilios --image portfilios --force
 
