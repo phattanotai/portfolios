91e2fb576a336bccbcdf8c41bd941d814694524e
phattanothai <zeref38@gmail,com>
2022-05-01 20:26:10 +0700

    up

--- src/Components/VersionInfo.tsx
+++ src/Components/VersionInfo.tsx
@@ -1,6 +1,5 @@
-import { FC } from "react";
-
 const VersionInfo = ({ versionRef }: any) => {
+  console.log(process.env.APP_VERSION_INFO);
   const info = process.env.APP_VERSION_INFO || "v1.0.0";
   return (
     <div className="versionApp" ref={versionRef}>
